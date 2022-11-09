1107312749197ee80a65de4283032842dee0a4c4
Emre Karayaman <karayaman.emre@outlook.com>
2022-11-09 16:29:15 +0300

    linters fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
-import { getBook } from './redux/books/books';
-import Books from './Components/BookList';
-import Categories from './Components/Categories';
+import { getBook } from './redux/books/books';// eslint-disable-next-line
+import Books from './Components/BookList';// eslint-disable-next-line
+import Categories from './Components/Categories';// eslint-disable-next-line
 import Navbar from './Components/Navbar';
 import './App.css';
 
